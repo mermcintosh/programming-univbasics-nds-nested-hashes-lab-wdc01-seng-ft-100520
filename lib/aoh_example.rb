e2242d49dc2a776c30a3d3b89d268e02f1f948c9
Meredith McIntosh <meredith@mcintosh.io>
2020-09-21 19:17:23 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -8,12 +8,17 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
   infinite_Jest = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
-  # Build an array that contains (or, "nests") the constants into a single
-  # Array. Ruby constants are denoted by ALL_CAPS
 end
 assembled_aoh
 
 def literal_aoh
+  infinite_Jest = [
+    { name:  "Don Gately", occupation: "Live-in Staff" },
+    { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+    { name:  "Pat Monteseian", occupation: "Staff" },
+    { name:  "Kate Gompert", occupation: "None" },
+    { name:  "Bruce Green", occupation: "Fan of Mildred" },
+    ]
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
