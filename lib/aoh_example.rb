ff329e7f40f8c52e8d14541ee7340c08d552aee5
Meredith McIntosh <meredith@mcintosh.io>
2020-09-21 19:44:48 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -20,21 +20,21 @@ def assembled_aoh
 end
 assembled_aoh
 
-def literal_aoh
-  infinite_Jest = [
-    { name:  "Don Gately", occupation: "Live-in Staff" },
-    { name:  "Joelle van Dyne", occupation: "Radio Personality" },
-    { name:  "Pat Monteseian", occupation: "Staff" },
-    { name:  "Kate Gompert", occupation: "None" },
-    { name:  "Bruce Green", occupation: "Fan of Mildred" },
-    ]
-end
-literal_aoh
+# def literal_aoh
+#   infinite_Jest = [
+#     { name:  "Don Gately", occupation: "Live-in Staff" },
+#     { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+#     { name:  "Pat Monteseian", occupation: "Staff" },
+#     { name:  "Kate Gompert", occupation: "None" },
+#     { name:  "Bruce Green", occupation: "Fan of Mildred" },
+#     ]
+# end
+# literal_aoh
 
-def aoh_lookup(aoh, row, key)
-  assembled_aoh[2][:name]
-end
-aoh_lookup(assembled_aoh, [2], [:name])
+# def aoh_lookup(aoh, row, key)
+#   assembled_aoh[2][:name]
+# end
+# aoh_lookup(assembled_aoh, [2], [:name])
 
 def aoh_update(aoh, row, key, new_value)
   aoh = infinite_Jest
