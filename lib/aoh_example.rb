b3aac6095ba388133ddb58412cd948eea461801b
Meredith McIntosh <meredith@mcintosh.io>
2020-09-21 19:47:13 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -45,7 +45,7 @@ def aoh_update(aoh, row, key, new_value)
     { name:  "Kate Gompert", occupation: "None" },
     { name:  "Bruce Green", occupation: "Fan of Mildred" },
     ]
-    infinite_Jest[row][key] = new_value
+    infinite_Jest[row][key]=> new_value
     binding.pry
 end
 aoh_update(infinite_Jest, [0], [:name], "Meredith McIntosh")
