ceaae3b2fb0cb43ceeccba0b6e89523b7cb405ac
Meredith McIntosh <meredith@mcintosh.io>
2020-09-21 19:37:12 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -14,26 +14,26 @@ infinite_Jest = [
     { name:  "Bruce Green", occupation: "Fan of Mildred" },
     ]
 
-def assembled_aoh
-  infinite_Jest = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
-end
-assembled_aoh
+# def assembled_aoh
+#   infinite_Jest = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+# end
+# assembled_aoh
 
-def literal_aoh
-  infinite_Jest = [
-    { name:  "Don Gately", occupation: "Live-in Staff" },
-    { name:  "Joelle van Dyne", occupation: "Radio Personality" },
-    { name:  "Pat Monteseian", occupation: "Staff" },
-    { name:  "Kate Gompert", occupation: "None" },
-    { name:  "Bruce Green", occupation: "Fan of Mildred" },
-    ]
-end
-literal_aoh
+# def literal_aoh
+#   infinite_Jest = [
+#     { name:  "Don Gately", occupation: "Live-in Staff" },
+#     { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+#     { name:  "Pat Monteseian", occupation: "Staff" },
+#     { name:  "Kate Gompert", occupation: "None" },
+#     { name:  "Bruce Green", occupation: "Fan of Mildred" },
+#     ]
+# end
+# literal_aoh
 
-def aoh_lookup(aoh, row, key)
-  assembled_aoh[2][:name]
-end
-aoh_lookup(assembled_aoh, [2], [:name])
+# def aoh_lookup(aoh, row, key)
+#   assembled_aoh[2][:name]
+# end
+# aoh_lookup(assembled_aoh, [2], [:name])
 
 def aoh_update(aoh, row, key, new_value)
 p assembled_aoh
